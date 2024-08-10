bfc458260342430306ea9550796dbfc82bfd952e
AVidhanR <itsvidhanreddy@gmail.com>
2024-08-10 13:14:09 +0530

    Update main.py
    Imported the visualization packages and the classification models

--- app/main.py
+++ app/main.py
@@ -3,3 +3,20 @@
 import warnings
 import io
 
+# visualization packages
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
