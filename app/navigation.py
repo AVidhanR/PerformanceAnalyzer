8618eb7db06092e6f51929189b3b304b3687e16d
AVidhanR <itsvidhanreddy@gmail.com>
2024-11-14 12:24:51 +0530

    Create navigation.py

---
+++ app/navigation.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(page_icon=":bar_chart:")
+
+st.markdown(
+    """
+    <style>
+     body, p, h1, h2, h3, h4, h5, li, ul, ol{
+        font-family: 'Cascadia Code';
+     }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.navigation({
+    "Home": [
+        st.Page("main.py", title="Performance Analyzer"),
+    ],
+    "About": [
+        st.Page("about.py", title="About us"),
+    ],
+}).run()
