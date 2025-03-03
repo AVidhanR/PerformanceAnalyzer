34f4d727cc95641687c629a24f19d7870e0b7053
AVidhanR <itsvidhanreddy@gmail.com>
2025-03-03 21:56:23 +0530

    Update main.py

--- app/pages/main.py
+++ app/pages/main.py
@@ -16,7 +16,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
 
 # for required data and content for the models
 from content_switcher import content_switcher
